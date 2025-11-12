33bc7253527616b1713bc0647a34836e072cedf5
nikita-kelwada <nikita.kelwada.01@gmail.com>
2025-11-12 07:03:18 +0000

    fixed PA3

--- PA3/pool.cc
+++ PA3/pool.cc
@@ -29,9 +29,10 @@ ThreadPool::~ThreadPool() {
 
 //TODO -> DONE
 void ThreadPool::SubmitTask(const string &name, Task *task) {
-    unique_lock<mutex> lock(mtx); //needed for locking the queue
+    mtx.lock();; //needed for locking the queue
 
     if (done){ //check if pool is stopped
+        mtx.unlock();
         cout << "Cannot added task to queue" << endl;
         return;
     }
@@ -40,49 +41,43 @@ void ThreadPool::SubmitTask(const string &name, Task *task) {
     queue.push_back(task); //add task to queue
     num_tasks_unserviced++; //increment unserviced task count
 
-    lock.unlock(); //unlock before notifying
-    cout << "Added task" << endl;
-    cv.notify_one(); //notify one thread that a task is available
-
+    mtx.unlock(); //unlock before notifying
+    cout << "Added task " << name << endl;
 }
 
 //TODO -> DONE
 void ThreadPool::run_thread() {
     while (true) {
-        Task *task = nullptr;
-        { //start scope block for lock
-            unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this]() { return done || !queue.empty(); }); //wait until there's a task or done is true
 
-            //TODO1: if done and no tasks left, break
-            if (done && queue.empty()) {//check if done and no tasks left
-                lock.unlock(); //unlock before breaking
-                cout << "Stopping thread " << this_thread::get_id() << endl; 
-                break;
-            }
-
-            //TODO2: if no tasks left, continue
-            if (queue.empty()) { //check if no tasks left
-                continue;
-            }
-            //TODO3: get task from queue, remove it from queue, and run it
-            task = queue.front(); //get task from queue
-            queue.erase(queue.begin()); //remove it from queue
-            task->running = true; //mark task as running
-        }//end scope block for lock
-
-            cout << "Started task " << task->name << endl;
-            task->Run();
-            cout << "Finished task " << task->name << endl;
-
-            { //start scope block for lock
-                unique_lock<mutex> lock(mtx);
-                task->running = false; //mark task as not running
-                num_tasks_unserviced--; //decrement unserviced task count
-            } //end scope block for lock
-            //TODO4: delete task
-            delete task; //delete task
+        mtx.lock(); //lock the queue
+        if (done && queue.empty()) { //check if done and no tasks left
+            mtx.unlock(); //unlock before breaking
+            cout << "Stopping thread " << this_thread::get_id() << endl;
+            break;
         }
+        
+        if (queue.empty()) { //check if no tasks in queue
+            mtx.unlock();
+            this_thread::sleep_for(chrono::milliseconds(10));
+            continue;
+        }
+
+        Task *task = queue.front(); //get the first task
+        queue.erase(queue.begin()); //remove it from the queue
+        task->running = true;
+        mtx.unlock();
+
+        cout << "Started task " << task->name << endl;
+        task->Run();
+        cout << "Finished task " << task->name << endl;
+
+        mtx.lock(); //lock to update task status
+        task->running = false; //mark task as not running
+        num_tasks_unserviced--; //decrement unserviced task count
+        mtx.unlock(); //unlock after updating
+
+        delete task;
+    }
 }
 
 
@@ -102,17 +97,13 @@ void ThreadPool::remove_task(Task *t) {
 
 //TODO -> DONE
 void ThreadPool::Stop() {
-    //TODO: Delete threads, but remember to wait for them to finish first
     cout << "Called Stop()" << endl;
     
-    {//scope block for lock
-        unique_lock<mutex> lock(mtx);
-        done = true;
-    }//scope block ends, lock is released
+    mtx.lock(); //lock to set done flag
+    done = true;
+    mtx.unlock();
     
-    cv.notify_all();
-    
-    for (thread *t : threads) {
+    for (thread *t : threads) { //join all threads
         if (t->joinable()) {
             t->join();
         }
